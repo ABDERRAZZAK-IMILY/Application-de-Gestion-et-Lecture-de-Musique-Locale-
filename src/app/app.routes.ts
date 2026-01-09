d76e5a50c96561a5d839916dad69475ddb6daa55
IMILY ABDERRAZZK <imily2024@gmail.com>
2026-01-09 14:34:53 +0100

    feat: update routing configuration for library and track details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,21 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: 'labrary',
-    loadComponent: () => import('./pages/labrary/labrary').then(m => m.Labrary)
-    },
-    {
-        path: 'track',
-        loadComponent: () => import('./pages/track/track').then(a => a.Track)
-    }
-
+{
+    path: '',
+    redirectTo: 'library',
+    pathMatch: 'full'
+  },
+  {
+    path: 'library',
+    loadComponent: () => import('./pages/library/library').then(m => m.Library)
+  },
+  {
+    path: 'track/:id',
+    loadComponent: () => import('./pages/track-detail/track-detail').then(m => m.TrackDetail)
+  },
+  {
+    path: 'add',
+    loadComponent: () => import('./components/track-form/track-form').then(m => m.TrackFormComponent)
+},
 ];
